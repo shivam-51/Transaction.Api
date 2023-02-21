0523a083b5f36b561d1494fd261f7138f329ff39
Shivam Singh <ssingh43@woolworths.com.au>
2023-02-21 12:12:43 +0530

    Added pubsub.yaml

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -12,20 +12,13 @@ namespace Transaction.Api.Controllers;
 public class TransactionController : Controller
 {
     private readonly IRepository<Transactions> _repository;
-    // private readonly DaprClient _daprClient;
+    private readonly DaprClient _daprClient;
 
-    public TransactionController(IRepository<Transactions> repository)
+    public TransactionController(IRepository<Transactions> repository, DaprClient daprClient)
     {
         _repository = repository;
-        // _daprClient = daprClient;
+        _daprClient = daprClient;
     }
-
-    // [HttpGet]
-    // public async Task<IEnumerable<TransactionDto>> GetAll([FromQuery] PagingParams pagingParms)
-    // {
-    //     var items = (await _repository.GetAllAsync(pagingParms)).Select(item => item.AsDto());
-    //     return items;
-    // }
 
     [HttpGet]
     public async Task<IEnumerable<TransactionDto>> GetWithFilters([FromQuery] PagingParams pagingParams, [FromQuery] QueryParams queryParams)
@@ -43,21 +36,10 @@ public class TransactionController : Controller
         if (item == null)
             return NotFound();
         
-        using var _daprClient = new DaprClientBuilder().Build();
-
-        
-        // process the received payload here
         string PUBSUB_NAME = "servicebusdapr";
-        string TOPIC_NAME = "first";
+        string TOPIC_NAME = "daprtopic";
         var transaction = new Transactions();
-        //Random random = new Random();
-        //transaction.id = JsonSerializer.Serialize(payload); 
-        //transaction.created_at = DateTime.Now;
-        CancellationTokenSource source = new CancellationTokenSource();
-        CancellationToken cancellationToken = source.Token;
-        // using var client = new DaprClientBuilder().Build();
-        //Using Dapr SDK to publish a topic
-        await _daprClient.PublishEventAsync(PUBSUB_NAME, TOPIC_NAME, transaction);
+        await _daprClient.PublishEventAsync(PUBSUB_NAME, TOPIC_NAME, "transaction");
         Console.WriteLine("Published data: " + transaction.Id);
         
         return item.AsDto();
--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddControllers(
             options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
         });
 
+builder.Services.AddControllers().AddDapr();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -24,11 +26,11 @@ builder.Services.AddMongo().AddMongoRepository<Transactions>("fourth");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
